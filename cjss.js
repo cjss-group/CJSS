5b949d6eca1eba021f35023a9ac42026e18b199e
Charlie Harding <xsanda@me.com>
2019-06-19 01:47:03 +0200

    Simplify getPureProperty

--- cjss.js
+++ cjss.js
@@ -2,7 +2,7 @@
 (() => {
 
   /**
-   * Get value of a rule's property and remove surrounding parentheses.
+   * Get value of a rule's property and remove surrounding parentheses, if present.
    * @param {CSSStyleRule} rule The CSSStyleRule, which to select from.
    * @param {String} propertyName The name/key which to select.
    * @returns {String} The contents of the given property, or empty if no such
@@ -10,7 +10,7 @@
    **/
   function getPureProperty(rule, propertyName) {
     const raw = rule.style.getPropertyValue(propertyName);
-    return raw.replace(/^\s*\(([\s\S]*)\)\s*$/g,'$1');
+    return raw.trim().replace(/^\(([\s\S]*)\)$/g,'$1');
   }
 
   /**

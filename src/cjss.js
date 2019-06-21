96b756af503c0e7f896503b2d078e93b6593eac2
Charlie Harding <xsanda@me.com>
2019-06-21 14:19:16 +0200

    Extract rule processing to a separate method

--- src/cjss.js
+++ src/cjss.js
@@ -3,6 +3,43 @@ import safeEval from './safeEval';
 import ruleList from './ruleList';
 
 /**
+ * Run one CJSS rule, handling the properties `--html`, `--js` and `--data`.
+ *
+ * @param {CSSRule} rule The rule to parse.
+ */
+function processRule(rule) {
+  const selector = rule.style.parentRule.selectorText;
+  const elements = document.querySelectorAll(selector);
+
+  const js = getPureProperty(rule, '--js');
+  const html = getPureProperty(rule, '--html');
+  let data = getPureProperty(rule, '--data');
+
+  data = data ? JSON.parse(`{${data}}`) : {};
+
+  if (html) {
+    for (let element of elements) {
+      element.innerHTML = safeEval(
+        `return (\`${ html }\`)`,
+        {
+          data,
+          yield: element.innerHTML
+        },
+        element
+      );
+    }
+  }
+
+  if (js) {
+    if (selector === 'script') {
+      safeEval(js, { data });
+    } else for (let element of elements) {
+      safeEval(js, { data }, element);
+    }
+  }
+}
+
+/**
    * Runs CJSS rules - CSS rules with the special properties `--html`,
    * `--js` and `--data`.
    *
@@ -19,38 +56,7 @@ export default function cjss(styleSheet) {
     }
 
     else if (ruleName === 'CSSStyleRule') {
-      const selector = rule.style.parentRule.selectorText;
-      const elements = document.querySelectorAll(selector);
-
-      const js = getPureProperty(rule, '--js');
-      const html = getPureProperty(rule, '--html');
-      let data = getPureProperty(rule, '--data');
-
-      data = data ? JSON.parse(`{${data}}`) : {};
-
-      if (html) {
-        for (let element of elements) {
-          element.innerHTML = safeEval(
-            `return (\`${ html }\`)`,
-            {
-              data,
-              yield: element.innerHTML
-            },
-            element
-          );
-        }
-      }
-
-      if (js) {
-        if (selector === 'script') {
-          safeEval(js, { data });
-          continue;
-        }
-
-        for (let element of elements) {
-          safeEval(js, { data }, element);
-        }
-      }
+      processRule(rule);
     }
   }
 }

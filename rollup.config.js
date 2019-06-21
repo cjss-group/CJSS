eb63ec280732ea44713fc96ec32cac734d6b46c5
Charlie Harding <charlie_harding@icloud.com>
2019-06-22 01:30:39 +0200

    Output both minified and unminified files

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,23 @@
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: './src/index.js',
-  plugins: [terser()],
-  output: [
-    {
-      file: './dist/cjss.min.js',
-      format: 'iife',
-      name: 'cjss',
-    },
-    {
+export default [
+  {
+    input: './src/index.js',
+    plugins: [],
+    output: {
       file: './dist/cjss.js',
       format: 'iife',
       name: 'cjss',
       sourcemap: 'inline',
     },
-  ],
-};
+  },
+  {
+    input: './src/index.js',
+    plugins: [terser()],
+    output: {
+      file: './dist/cjss.min.js',
+      format: 'iife',
+      name: 'cjss',
+    },
+  },
+];
